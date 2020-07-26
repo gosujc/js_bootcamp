54e9ec595bd43fdb388e68017d573df6d5a4d314
David Kim <scriptura7773@gmail.com>
2020-07-25 23:00:11 -0700

    more practice

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -9,23 +9,4 @@ const notes = [ {
     body: 'Get a new seat'
 }]
 
-// DOM - Document Object Model
-
-
-// Query and remove
-// const p = document.querySelector('p')
-// p.remove()
-
-// Query all and remove
-const ps = document.querySelectorAll('p')
-
-ps.forEach(function (p) {
-    p.textContent = '******'
-    //console.log(p.textContent)
-    // p.remove()
-})
-
-// Add a new element
-const newParagraph = document.createElement('p')
-newParagraph.textContent = 'This is a new element from Javascript.'
-document.querySelector('body').appendChild(newParagraph)
+document.querySelector('button')
