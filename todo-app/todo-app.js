93efecaa78a26ceee88841a1332d011f324b87f8
David Kim <scriptura7773@gmail.com>
2020-07-27 21:29:20 -0700

    finishing up

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -22,48 +22,33 @@ const todos = [ {
 
 // Starts
 
+const filters = {
+    searchText = ''
+}
+
 const renderedNotes = function (todos, filters) {
     const filteredNotes = todos.filter(function (todo) {
         return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
     })
 
-    document.querySelector('#todos').innerHTML('')
+    document.querySelector('#todos').innerHTML = ''
 
     filteredNotes.forEach(function (todo) {
-    const newText = document.createElement('p')
-    newText.textContent = todo.text
-    document.querySelector('#todos').appendChild(filteredNotes)
+        const newText = document.createElement('p')
+        newText.textContent = todo.text
+        document.querySelector('#todos').appendChild(newText)
     })
 }
 
 renderedNotes(todos, filters)
-
-
-const newParagraph = document.createElement('h2')
-newParagraph.textContent = `You have ${falsePara.length} todos left.`
-document.querySelector('body').appendChild(newParagraph)
-
-todos.forEach(function (todo) {
-    const p = document.createElement('p')
-    p.textContent = todo.text
-    document.querySelector('body').appendChild(p)
-})
 // Ends
 
 // You have 2 todos left (p element)
 // Add a p for each todo above (use text value)
 
-
-// Listen for new todo creation. 
-
-document.querySelector('#add-todo').addEventListener('click', function (e) {
-    console.log('Add a new todo...')
-})
-
 // Listen for todo text change
 
 document.querySelector('#new-todo-text').addEventListener('input', function (e) {
     filters.searchText = e.target.value
-    renderNotes(todos, filters)//.target gives us access to the input to access it's value property. 
+    renderedNotes(todos, filters)//.target gives us access to the input to access it's value property. 
 })
-
