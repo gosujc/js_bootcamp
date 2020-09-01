26c40a0e7e0255018081c64f9287d273c855af16
David Kim <david_kim@Davids-MBP.home>
2020-08-31 20:22:25 -0700

    updating dir

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,4 +1,4 @@
-const notes = getSavedNotes()
+let notes = getSavedNotes()
 
 const filters = {
     searchText: ''
@@ -26,3 +26,38 @@ document.querySelector('#search-text').addEventListener('input', function (e) {
 document.querySelector('#filter-by').addEventListener('change', function (e) {
     console.log(e.target.value)
 })
+
+window.addEventListener('storage', function(e) {
+    if (e.key === 'notes') {
+       notes = JSON.parse(e.newValue)
+       renderNotes(notes, filters)
+    }
+})
+
+// Unix Epoch - January 1st 1970 00:00:00
+
+const now = new Date()
+const timestamp = now.getTime()
+
+const myDate = new Date(timestamp)
+console.log(myDate.getFullYear())
+
+// console.log(`Year: ${now.getFullYear()}`)
+// console.log(`Month: ${now.getMonth()}`)
+// console.log(`Day of Month: ${now.getDate()}`)
+// console.log(`Hours: ${now.getHours()}`)
+// console.log(`Minutes: ${now.getMinutes()}`)
+// console.log(`Seconds: ${now.getSeconds()}`)
+
+// 1. Create two dates in the past (use string for Date)
+
+const dateOne = new Date('February 14 1993 09:40:27')
+const dateTwo = new Date('December 25 2007 10:45:21')
+// 2. Get time stamps for both
+const stampOne = dateOne.getTime()
+const stampTwo = dateTwo.getTime()
+
+// 3. Figure out which is first and print its time (use toString)
+if (stampOne > stampTwo) { 
+    return stampOne
+} else if {}
--- notes-app/notes-edit.js
+++ notes-app/notes-edit.js
@@ -1,10 +1,11 @@
 const noteId = location.hash.substring(1)
-const notes = getSavedNotes()
 const noteTitle = document.querySelector('#note-title')
 const noteBody = document.querySelector('#note-body')
 const removeButton = document.querySelector('#remove-note')
 
-const note = notes.find(function (note) {
+let notes = getSavedNotes()
+
+let note = notes.find(function (note) {
     return note.id === noteId
 })
 if (note === undefined) { 
@@ -28,5 +29,20 @@ removeButton.addEventListener('click', function (e) {
    removeNote(note.id)
    saveNotes(notes)
    location.assign('/index.html')
+})
+
+window.addEventListener('storage', function(e) {
+   if (e.key === 'notes') {
+       notes = JSON.parse(e.newValue)
+        note = notes.find(function (note) {
+        return note.id === noteId
+    })
+    if (note === undefined) { 
+        location.assign('/index.html')
+    }
+
+    noteTitle.value = note.title
+    noteBody.value = note.body
+   }
 })
 
